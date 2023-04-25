1eb3b3ffd5271787219320433dff6fb21cac65c0
jvsobraz <jvsobraz@hotmail.com>
2023-04-25 09:14:31 -0300

    Update main.jsx

--- quiz/src/main.jsx
+++ quiz/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { QuizProvider } from "./context/quiz";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QuizProvider>
+      <App />
+    </QuizProvider>
+  </React.StrictMode>
+);
