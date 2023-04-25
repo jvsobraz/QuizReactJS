2d6a7f607b434a520a6c84769afadb33ce7730b8
jvsobraz <jvsobraz@hotmail.com>
2023-04-25 09:13:06 -0300

    Update App.jsx

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useContext, useEffect } from "react";
+import { QuizContext } from "./context/quiz";
+
+import Welcome from "./components/Welcome";
+import Question from "./components/Question";
+import GameOver from "./components/GameOver";
+
+import PickCategory from "./components/PickCategory";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
+    </div>
+  );
 }
 
-export default App
+export default App;
